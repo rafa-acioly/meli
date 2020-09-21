2748f9a0e0d28506b87d3a7e6b180ba6ab2292be
aciolyr@gmail.com <aciolyr@gmail.com>
2020-09-21 10:36:17 -0300

    adds controller do update product on meli

--- app/Http/Controllers/WoocommerceWebhookController.php
+++ app/Http/Controllers/WoocommerceWebhookController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Entities\WoocommerceProduct;
 use App\Resources\Woocommerce;
-use Illuminate\Http\Response;
 
 use App\Http\Requests\WoocommerceProductRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class WoocommerceWebhookController extends Controller
 {
@@ -22,9 +24,22 @@ class WoocommerceWebhookController extends Controller
         $event = $request->header(Woocommerce::EVENT_KEY);
         $eventType = $request->header(Woocommerce::EVENT_TYPE_KEY);
 
+        $product = new WoocommerceProduct($request->all());
+        $meli = new Meli('APP-ID', 'SECRET-ID');
+        $auth = new AuthorizationService($meli);
+        $announcement = new Announcement($auth);
+
+        $productData = [
+            'title' => $product->getTitle(),
+            'available_quantity' => $product->getAvailableQuantity(),
+            'price' => $product->getPrice(),
+        ];
+
+        $integratedProduct = Auth::user()->products()->where('woo_product_id', $product->id);
+        $response = $announcement->update($integratedProduct->meli_product_id, $productData);
+
         return response()->json([
-            'event' => $event,
-            'type' => $eventType
-        ], Response::HTTP_OK);
+            'permalink' => $response->getPermalink(),
+        ], Response::HTTP_ACCEPTED);
     }
 }
